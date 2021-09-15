07d52ee483eada02ab7d0f628dfef4876524c49e
OliveiraSouzaBruno <oliveira_bruno2018@hotmail.com>
2021-09-15 17:32:14 -0300

    Update automacao_residencial.js

--- automacao_residencial.js
+++ automacao_residencial.js
@@ -1,13 +1,16 @@
+
+
+
 function AlteraRele1(){
-    var estado = document.getElementById("estado").innerHTML;
+    var estado = document.getElementById("cozinhaPlafonIsOff").innerHTML;
     if(estado === "0"){
-        document.getElementById("estado").innerHTML="1";
-        document.getElementById("rele").innerHTML="<div class='rele1_ligado'></div>";
-		document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desliga</a>";
+        document.getElementById("cozinhaPlafonIsOff").innerHTML="1";
+
+		document.getElementById("cozinhaPlafon").innerHTML="<a href='/?cozinhaPlafon=desligar'> <div class='p-1 m-1 border btn btn-outline-info border-info  fw-bold'> Liga/Desliga</div></a>";
     } else {
-        document.getElementById("estado").innerHTML="0";
-        document.getElementById("rele").innerHTML="<div class='rele1_desligado'></div>";
-        document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>Liga</a>";
+        document.getElementById("cozinhaPlafonIsOff").innerHTML="0";
+
+        document.getElementById("cozinhaPlafon").innerHTML="<a href='/?cozinhaPlafon=ligar'> <div class='p-1 m-1 border btn btn-outline-info border-info  fw-bold'> Liga/Desliga</div></a>";
     }
 }
 
@@ -15,7 +18,7 @@ function AlteraRele2(){
     var estado_2 = document.getElementById("estado_2").innerHTML;
     if(estado_2 === "0"){
         document.getElementById("estado_2").innerHTML="1";
-        document.getElementById("rele_2").innerHTML="<div class='rele2_ligado'></div>";
+
 		document.getElementById("botao_2").innerHTML="<a href='/?2_desligar' class='botao_2'>Desliga</a>";
     } else {
         document.getElementById("estado_2").innerHTML="0";
